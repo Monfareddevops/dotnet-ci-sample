d62eb4aa285300344fe7b4b08fb0e72f2b314d76
Ali Gholami Monfared <Monfaredevops@gmail.com>
2025-06-30 17:24:36 +0330

    Create UnitTest1.cs

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MyApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
